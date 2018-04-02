9230a4c8f6e346cae1e39079461acd751d8fe279
Bruno Oliveira <bbrioli@gmail.com>
2018-04-02 11:42:16 -0300

    Criação do diretório templates do app cursos e outros pequenos ajustes

--- treineme/settings.py
+++ treineme/settings.py
@@ -79,16 +79,15 @@ WSGI_APPLICATION = 'wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-
-
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'treineme',
         'USER': 'treinemeuser',
         'PASSWORD': 'a1b2c3d4@@',
         'HOST': '',
         'PORT': '5432',
+
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
